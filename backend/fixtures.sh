cd7093d2a2b2f0fa870d7163b34664a5c01b16af
ynchk1 <saspery@yandex.ru>
2025-07-05 18:24:39 +0300

    some changes

--- backend/fixtures.sh
+++ backend/fixtures.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 python manage.py makemigrations 
+python manage.py flush --noinput
 python manage.py migrate 
 python manage.py loaddata users 
 python manage.py loaddata connections 
